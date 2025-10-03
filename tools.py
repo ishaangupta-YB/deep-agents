e1ccc16a12abf1abf693738c96cf497fddf93ca0
Ishaan Gupta <ishaangupta999@gmail.com>
2025-10-04 02:33:28 +0530

    added tools

---
+++ tools.py
@@ -0,0 +1,25 @@
+import os
+from typing import Literal
+from tavily import TavilyClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+tavily_client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
+
+def internet_search(
+    query: str,
+    max_results: int = 5,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = True,
+):
+    """Run a web search"""
+    search_docs = tavily_client.search(
+        query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic,
+    )
+    return search_docs
+
+print(internet_search("who is ishaangupta-yb?"))
